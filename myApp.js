ccf67b20fdbe7f09cec309d2ba8a8dfc157fd21c
Eid Hachem <eidhachem1@gmail.com>
2024-07-02 13:51:47 +0300

    Add post requet

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   const method = req.method;
@@ -38,6 +41,17 @@ app.get('/:word/echo', (req, res, next) => {
   const word = req.params.word;
   res.json({ "echo": word });
   next();
+});
+
+app.route("/name").get((req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ "name": `${firstName} ${lastName}` });
+}).post((req, res) => {
+  const first = req.body.first;
+  const last = req.body.last;
+
+  res.json({ "name": `${first} ${last}` });
 });
 
 app.use("/public", express.static(__dirname + '/public'));
